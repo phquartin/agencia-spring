51644aa72c2613b4ca014981a4d5579ef0e896dc
phquartin <phquartinmatos@sempreceub.com>
2025-04-05 16:07:05 -0300

    refactor(destiny-model): ajustar mapeamento e validações
    - Adicionado `@NotBlank` na propriedade `name` para garantir que o campo não seja vazio ou em branco.
    - Modificado o mapeamento para `country` com `CascadeType.PERSIST` e alterado o nome do atributo de `id_country` para `country`.
    - Incluído `@JsonIgnore` na lista de `vacationPackages` para evitar problemas de serialização.

--- src/main/java/com/agencia/spring/model/destiny/DestinyModel.java
+++ src/main/java/com/agencia/spring/model/destiny/DestinyModel.java
@@ -2,10 +2,12 @@ package com.agencia.spring.model.destiny;
 
 import com.agencia.spring.model.countries.CountryModel;
 import com.agencia.spring.model.vpackage.VacationPackageModel;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
 
 import java.util.List;
 
@@ -20,17 +22,18 @@ public class DestinyModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // Um destino (cidade) possui um pais, entretanto um pais possui varios destinos
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "id_country", nullable = false)
-    private CountryModel id_country;
+    private CountryModel country;
 
     @Column(nullable = false)
+    @NotBlank(message = "O campo nome nao pode estar vazio ou em branco!")
     private String name;
 
     private String description;
 
     @ManyToMany(mappedBy = "destinies")
+    @JsonIgnore
     private List<VacationPackageModel> vacationPackages;
 
 }
