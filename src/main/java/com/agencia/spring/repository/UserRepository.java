bae41f3c3c0fcb25cf2967be36914699e1497d56
phquartin <phquartinmatos@sempreceub.com>
2025-04-05 11:48:27 -0300

    feat: adicionar UserRepository para acesso a dados do usuário
    - Criada a interface UserRepository com extensão de JpaRepository.
    - Adicionado método para buscar usuários pelo nome de usuário.
    - Facilita a interação com a base de dados relacionada a usuários.

---
+++ src/main/java/com/agencia/spring/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.agencia.spring.repository;
+
+import com.agencia.spring.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+    UserModel findByUsername(String username);
+}
